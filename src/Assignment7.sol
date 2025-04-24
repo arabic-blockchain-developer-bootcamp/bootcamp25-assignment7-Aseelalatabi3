9fab0c4b661a7f763acc6deadf11c2cf68663a69
aseelalatabi3 <aseelalatabi3@gmail.com>
2025-04-25 00:38:32 +0300

    Fix mint function and update constructor parameters

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
-    // Constructor to initialize the token
-    // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    // Constructor with token name and symbol
+    constructor() ERC20("MyToken", "MTK") {}
 
     // Function to mint tokens
-    // Fill in the visibility modifier to be external
-    // Set params as to address and amount
-    function mint() {
-        // Fill in the logic
-        // call _mint function with to and amount params
+    // Added 'external' visibility modifier
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 }
